835d7cab113711332f630db09cda8ff93c855848
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-05-10 17:40:21 +0530

    edited Navbar.js

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+export default function Navbar() {
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+
+                </div>
+            </nav>
+        </>
+    )
+}
